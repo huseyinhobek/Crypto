14a56a09f4045f69c3de478bfea5fbf3f8498c0d
Hüseyin HÖBEK <hsynhbk@icloud.com>
2023-01-27 10:45:13 +0300

    first commit

---
+++ CryptoApp/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  CryptoApp
+//
+//  Created by Hüseyin HÖBEK on 26.01.2023.
+//
+
+import Foundation
---
+++ CryptoApp/WebService.swift
@@ -0,0 +1,8 @@
+//
+//  WebService.swift
+//  CryptoApp
+//
+//  Created by Hüseyin HÖBEK on 26.01.2023.
+//
+
+import Foundation
